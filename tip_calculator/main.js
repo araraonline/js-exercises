eb2c3ae0d19c50325bcbe23e48deed796ac70ac2
araraonline <andre0lra@gmail.com>
2018-05-18 20:55:39 -0300

    Reject bad values and round the numbers

--- tip_calculator/main.js
+++ tip_calculator/main.js
@@ -8,12 +8,42 @@ calculateBtn.addEventListener('click', function() {
     bill = Number(billInp.value);
     tippct = Number(tippctInp.value);
 
-    tip = tippct * bill / 100;
-    total = tip + bill;
+    // values must be a finite number
+    if (!Number.isFinite(bill) |
+        !Number.isFinite(tippct)) {
+        alert("Your inputs are not correct!");
+    }
 
-    tipInp.value = String(tip);
-    totalInp.value = String(total);
+    tip = round((tippct / 100) * bill);
+    total = round(bill + tip);
+
+    tipInp.value = tip.toFixed(2);
+    totalInp.value = total.toFixed(2);
 
     // defocus button
     window.focus()
 });
+
+
+// Rounding function
+//
+// Rounding is complicated in javascript, as many
+// problems may arise from the floating-point approximations
+//
+// This function below is from an algorithm stolen from
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#Decimal_rounding
+//
+// It will round values to 2 decimal places
+//
+function round(value) {
+    // 2 decimal places
+    exp = -2;
+
+    // Shift
+    value = value.toString().split('e');
+    value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
